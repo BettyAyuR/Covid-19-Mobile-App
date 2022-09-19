022517174d6a2f9825c5b0be7ebf56c26cf94fb4
BettyAyuR <113825338+BettyAyuR@users.noreply.github.com>
2022-09-19 10:27:53 +0700

    Add files via upload

---
+++ TentangActivity.java
@@ -0,0 +1,14 @@
+package com.example.a17416255201107_covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TentangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tentang_activity);
+    }
+}
